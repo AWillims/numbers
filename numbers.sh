deb5a6ad5d46683258ac0a0064e6ef043913c60b
Avery Williams <awilliams@chapman.edu>
2025-10-22 20:07:36 +0000

    ok

--- numbers.sh
+++ numbers.sh
@@ -5,12 +5,12 @@
 
 read -r "Type in a positive number: " number_input 
 
-if (( $number_input <= 0)); then
+if (( number_input <= 0)); then
     echo "You cannot input anything lower than 1"
     fi
 
 if $number_input -lt 10; then
-    for i in $(seq 1 $number_input); do
+    for i in $(seq 1 "$number_input"); do
         if (( number_input < 10 )); then
             if (( i % 2 == 0 )); then
                 echo "$i: Even"
@@ -21,9 +21,9 @@ if $number_input -lt 10; then
     done
 else
     echo -n "Odd: " 
-    seq 1 2 $number_input | tr '\n' ' '
+    seq 1 2 "$number_input" | tr '\n' ' '
     echo
     echo -n "Even: "
-    seq 2 2 $number_input | tr '\n' ' '
+    seq 2 2 "$number_input" | tr '\n' ' '
     echo
 fi
