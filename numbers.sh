3ecef4836003145206dceaec91c73d52652605b5
Avery Williams <awilliams@chapman.edu>
2025-10-29 00:32:31 +0000

    numbers.sh

--- numbers.sh
+++ numbers.sh
@@ -3,24 +3,23 @@
 # Avery Williams
 # CENG298
 
-read -r "Type in a positive number: " number_input 
+read -r -p "Type in a positive number: " number_input
 
-if (( number_input <= 0)); then
+if (( number_input <= 0 )); then
     echo "You cannot input anything lower than 1"
-    fi
+    exit 1
+fi
 
-if $number_input -lt 10; then
+if (( number_input < 10 )); then
     for i in $(seq 1 "$number_input"); do
-        if (( number_input < 10 )); then
-            if (( i % 2 == 0 )); then
-                echo "$i: Even"
-            else
-                echo "$i: Odd"
-            fi
+        if (( i % 2 == 0 )); then
+            echo "$i: Even"
+        else
+            echo "$i: Odd"
         fi
     done
 else
-    echo -n "Odd: " 
+    echo -n "Odd: "
     seq 1 2 "$number_input" | tr '\n' ' '
     echo
     echo -n "Even: "
